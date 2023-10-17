ef41351159f0e326ba77b1f41183349256f72349
EAD\6119962-Hudson <hudsonjoubert.rodriguesdasilva@thomsonreuters.com>
2023-10-17 19:36:10 -0300

    add delete methods in RestaurantController and RestaurantRegistrationService

--- src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestaurantController.java
@@ -1,16 +1,20 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntityInUseException;
 import com.algaworks.algafood.domain.exception.EntityNotFoundException;
 import com.algaworks.algafood.domain.model.Restaurant;
 import com.algaworks.algafood.domain.repository.RestaurantRepository;
 import com.algaworks.algafood.domain.service.RestaurantRegistrationService;
 import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -52,6 +56,37 @@ public class RestaurantController {
       return ResponseEntity.status(HttpStatus.CREATED).body(restaurant);
     } catch (EntityNotFoundException e) {
       return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Restaurant restaurant) {
+
+    try {
+      Restaurant currentRestaurant = restaurantRepository.find(id);
+
+      if (currentRestaurant != null) {
+        BeanUtils.copyProperties(restaurant, currentRestaurant, "id");
+
+        currentRestaurant = restaurantRegistrationService.save(currentRestaurant);
+        return ResponseEntity.ok(currentRestaurant);
+      }
+      return ResponseEntity.notFound().build();
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Restaurant> delete(@PathVariable Long id) {
+    try {
+      restaurantRegistrationService.remove(id);
+
+      return ResponseEntity.noContent().build();
+    } catch (EntityNotFoundException e) {
+      return ResponseEntity.notFound().build();
+    } catch (EntityInUseException e) {
+      return ResponseEntity.status(HttpStatus.CONFLICT).build();
     }
   }
 
--- src/main/java/com/algaworks/algafood/domain/service/RestaurantRegistrationService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RestaurantRegistrationService.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.domain.service;
 
+import com.algaworks.algafood.domain.exception.EntityInUseException;
 import com.algaworks.algafood.domain.exception.EntityNotFoundException;
 import com.algaworks.algafood.domain.model.Kitchen;
 import com.algaworks.algafood.domain.model.Restaurant;
 import com.algaworks.algafood.domain.repository.KitchenRepository;
 import com.algaworks.algafood.domain.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -28,6 +31,16 @@ public class RestaurantRegistrationService {
     restaurant.setKitchen(kitchen);
 
     return restaurantRepository.save(restaurant);
+  }
+
+  public void remove(Long id) {
+    try {
+      restaurantRepository.remove(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new EntityNotFoundException("Restaurant not found!");
+    } catch (DataIntegrityViolationException e) {
+      throw new EntityInUseException("Restaurant cannot been removed!");
+    }
   }
 
 
